ddd129021e49085028ccd0a36308626c9d7abd63
Mohammadjavad Salehi <83529512+MohammadjavadSalehi@users.noreply.github.com>
2023-12-05 23:47:17 +0330

    add get urls library and fix bugs

--- components/Board.tsx
+++ components/Board.tsx
@@ -3,15 +3,16 @@
 import { useBoardStore } from "@/store/BoardStore";
 import { useEffect } from "react";
 import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
-import Column from "./Column";
+import ColumnComponent from "./Column";
+import { Column } from "@/typings";
 
 function Board() {
-  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+  const [board, getBoard, setBoardState, updateTodoInDb] = useBoardStore(
     (state) => [
       state.board,
       state.getBoard,
       state.setBoardState,
-      state.updateTodoInDB,
+      state.updateTodoInDb,
     ]
   );
 
@@ -22,20 +23,24 @@ function Board() {
   const handleOnDragEnd = (result: DropResult) => {
     const { destination, source, type } = result;
 
-    // Check if user dropped outside of board
+    // check if user dragged card outside board
     if (!destination) return;
 
-    // Handle column drag
+    // handle column drag
     if (type === "column") {
       const entries = Array.from(board.columns.entries());
       const [removed] = entries.splice(source.index, 1);
       entries.splice(destination.index, 0, removed);
+      // now re-arrange the map
       const rearrangedColumns = new Map(entries);
-      setBoardState({ ...board, columns: rearrangedColumns });
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
     }
 
-    // Handle card drag
-    // This step is needed as the indices are stored in the numbers as 0,1,2 etc. instead of IDs with DND library
+    // this step is needed as the indexes are stores
+    // as numbers 0,1,2 e.t.c. instead of id's with DND library
     const columns = Array.from(board.columns);
     const startColIndex = columns[Number(source.droppableId)];
     const finishColIndex = columns[Number(destination.droppableId)];
@@ -44,29 +49,32 @@ function Board() {
       id: startColIndex[0],
       todos: startColIndex[1].todos,
     };
+
     const finishCol: Column = {
       id: finishColIndex[0],
       todos: finishColIndex[1].todos,
     };
 
     if (!startCol || !finishCol) return;
+
     if (source.index === destination.index && startCol === finishCol) return;
 
     const newTodos = startCol.todos;
     const [todoMoved] = newTodos.splice(source.index, 1);
 
     if (startCol.id === finishCol.id) {
+      // same column task drag
       newTodos.splice(destination.index, 0, todoMoved);
       const newCol = {
         id: startCol.id,
         todos: newTodos,
       };
-
       const newColumns = new Map(board.columns);
       newColumns.set(startCol.id, newCol);
+
       setBoardState({ ...board, columns: newColumns });
     } else {
-      // dragging to another column
+      // dragging to a different column
       const finishTodos = Array.from(finishCol.todos);
       finishTodos.splice(destination.index, 0, todoMoved);
 
@@ -82,24 +90,29 @@ function Board() {
         todos: finishTodos,
       });
 
-      // Update in DB
-      updateTodoInDB(todoMoved, finishCol.id);
+      // update in db
+      updateTodoInDb(todoMoved, finishCol.id);
+
       setBoardState({ ...board, columns: newColumns });
     }
   };
 
-  console.log(board);
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
         {(provided) => (
           <div
-            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
             {...provided.droppableProps}
             ref={provided.innerRef}
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
           >
-            {Array.from(board.columns.entries()).map(([id, colunm], index) => (
-              <Column key={id} id={id} todos={colunm.todos} index={index} />
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <ColumnComponent
+                key={id}
+                id={id}
+                todos={column.todos}
+                index={index}
+              ></ColumnComponent>
             ))}
           </div>
         )}
